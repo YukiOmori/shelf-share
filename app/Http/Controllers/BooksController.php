5c4f91a488d57c7b083b789e845695cf85135924
Yuki Omori <ships.jet.black@gmail.com>
2017-12-10 09:21:46 +0000

    課題3終了: コントローラの作成完了

---
+++ app/Http/Controllers/BooksController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Book;
+use Validator;
+
+class BooksController extends Controller
+{
+    public function showBooksTable() {
+        $books = Book::orderBy('created_at', 'asc')->get();
+        return view('books', ['books' => $books]); // 質問：viewの第二引数の役割とここの記述の意味
+    }
+    
+    public function showBookData(Book $books) {
+       return view('booksedit', ['book' => $books]); 
+    }
+    //更新
+    public function update(Request $request) {
+       $validator = Validator::make($request->all(),
+                                ['id' => 'required',
+                                'item_name' => 'required | min:1 |max:255',
+                                'item_number' => 'required | min:1 | max: 3',
+                                'item_amount' => 'required | min:1 | max: 6',
+                                'author' => 'required | min:1 | max: 10',
+                                'publisher' => 'required | min:1 | max: 10',
+                                'published' => 'required']); 
+        if ($validator->fails()) {
+            return redirect('/')
+                        ->withInput()
+                        ->withErrors($validator);
+        }
+    
+        $books = Book::find($request->id);
+        
+        $books->item_name = $request->item_name;
+        $books->item_number = $request->item_number;
+        $books->item_amount = $request->item_amount;
+        $books->author = $request->author;
+        $books->publisher = $request->publisher;
+        $books->published = $request->published;
+        
+        $books->save();
+        return redirect('/');
+    }
+    
+    public function store(Request $request) {
+        $validator = Validator::make($request->all(), 
+                                ['item_name' => 'required | min:1 |max:255',
+                                'item_number' => 'required | min:1 | max: 3',
+                                'item_amount' => 'required | min:1 | max: 6',
+                                'author' => 'required | min:1 | max: 10',
+                                'publisher' => 'required | min:1 | max: 10',
+                                'published' => 'required']);
+    
+        if ($validator->fails()) {
+            return redirect('/')
+                        ->withInput()
+                        ->withErrors($validator);
+        }
+    
+        $books = new Book;
+        
+        $books->item_name = $request->item_name;
+        $books->item_number = $request->item_number;
+        $books->item_amount = $request->item_amount;
+        $books->author = $request->author;
+        $books->publisher = $request->publisher;
+        $books->published = $request->published;
+    
+        $books->save();
+        return redirect('/');
+    }
+    
+    public function delete (Book $book) {
+        $book->delete();
+        return redirect('/');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,74 +14,12 @@
 use App\Book;
 use Illuminate\Http\Request;
 
-Route::get('/', function () {
-    $books = Book::orderBy('created_at', 'asc')->get();
-    return view('books', ['books' => $books]); // 質問：viewの第二引数の役割とここの記述の意味
-});
+Route::get('/', 'BooksController@showBooksTable');
 
+Route::post('/books', 'BooksController@store');
 
-Route::post('/books/update', function (Request $request) {
-    $validator = Validator::make($request->all(), 
-                                ['item_name' => 'required | min:1 |max:255',
-                                'item_number' => 'required | min:1 | max: 3',
-                                'item_amount' => 'required | min:1 | max: 6',
-                                'author' => 'required | min:1 | max: 10',
-                                'publisher' => 'required | min:1 | max: 10',
-                                'published' => 'required']);
-    
-    if ($validator->fails()) {
-        return redirect('/')
-                    ->withInput()
-                    ->withErrors($validator);
-    }
-    
-    $books = new Book;
-    
-    $books->item_name = $request->item_name;
-    $books->item_number = $request->item_number;
-    $books->item_amount = $request->item_amount;
-    $books->author = $request->author;
-    $books->publisher = $request->publisher;
-    $books->published = $request->published;
+Route::post('/booksedit/{books}','BooksController@showBookData');
 
-    $books->save();
-    return redirect('/');
-});
+Route::post('/books/update', 'BooksController@update');
 
-Route::post('/booksedit/{books}', function (Book $books) {
-   
-   return view('booksedit', ['book' => $books]); 
-});
-
-Route::post('/books/update', function(Request $request) {
-   $validator = Validator::make($request->all(),
-                                ['id' => 'required',
-                                'item_name' => 'required | min:1 |max:255',
-                                'item_number' => 'required | min:1 | max: 3',
-                                'item_amount' => 'required | min:1 | max: 6',
-                                'author' => 'required | min:1 | max: 10',
-                                'publisher' => 'required | min:1 | max: 10',
-                                'published' => 'required']); 
-    if ($validator->fails()) {
-        return redirect('/')
-                    ->withInput()
-                    ->withErrors($validator);
-    }
-    
-    $books = Book::find($request->id);
-    
-    $books->item_name = $request->item_name;
-    $books->item_number = $request->item_number;
-    $books->item_amount = $request->item_amount;
-    $books->author = $request->author;
-    $books->publisher = $request->publisher;
-    $books->published = $request->published;
-    
-    $books->save();
-    return redirect('/');
-});
-
-Route::delete('/book/{book}', function (Book $book) {
-    $book->delete();
-    return redirect('/');
-});
+Route::delete('/book/{book}', 'BooksController@delete');
